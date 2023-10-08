a50bdded604d7634118bcb8492bc87b67bc95505
ellenjaiany <131263420+ellenjaiany@users.noreply.github.com>
2023-10-08 17:03:06 -0300

    Update perguntasDoDavid.sql

--- 3o-bimestre/perguntasDoDavid.sql
+++ 3o-bimestre/perguntasDoDavid.sql
@@ -1,13 +1,14 @@
-# Liste todos os hotéis que funcionam até às 22h
+# Liste todos os hotéis que funcionam até às 22h:
 SELECT * FROM hotel WHERE horario_termino = 22;
 
-# Lista o top 3 hotéis melhores avaliados
+# Lista o top 3 hotéis melhores avaliados:
 SELECT * FROM hotel ORDER BY avaliacao DESC LIMIT 3;
 
-# Liste o histórico de usos de um determinado usuário
-SELECT historico FROM cliente WHERE FK_usuario_c = 'dudalind'
+# Liste o histórico de usos de um determinado usuário:
+SELECT historico FROM cliente WHERE FK_usuario_c = '@dudalind'
   
-# Quais petshops um determinado animal de um determinado usuários ele já foi
+# Quais petshops um determinado animal de um determinado usuários ele já foi:
 
-# Quanto um determinado usuário já gastou no chatpet e quais foram as transações em ordem decrescente de valor
-SELECT SUM(valor) as soma FROM transacao WHERE FK_usuario_c = 'Debor4'
+
+# Quanto um determinado usuário já gastou no chatpet e quais foram as transações em ordem decrescente de valor:
+SELECT SUM(valor) as soma FROM transacao WHERE FK_usuario_c = '@Debor4'
