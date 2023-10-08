59f01a4fcb10688b4c5bcf6947152494e75c2be3
ellenjaiany <131263420+ellenjaiany@users.noreply.github.com>
2023-10-08 17:10:21 -0300

    Update perguntasDoDavid.sql

--- 3o-bimestre/perguntasDoDavid.sql
+++ 3o-bimestre/perguntasDoDavid.sql
@@ -5,10 +5,10 @@ SELECT * FROM hotel WHERE horario_termino = 22;
 SELECT * FROM hotel ORDER BY avaliacao DESC LIMIT 3;
 
 # Liste o histórico de usos de um determinado usuário:
-SELECT historico FROM cliente WHERE FK_usuario_c = '@dudalind'
+SELECT historico FROM cliente WHERE FK_usuario_c = '@dudalind';
   
 # Quais petshops um determinado animal de um determinado usuários ele já foi:
-SELECT historico FROM cliente WHERE historico = "PetShop%"
+SELECT historico FROM cliente WHERE historico = "PetShop%";
 
 # Quanto um determinado usuário já gastou no chatpet e quais foram as transações em ordem decrescente de valor:
-SELECT SUM(valor) as soma FROM transacao WHERE FK_usuario_c = '@Debor4'
+SELECT SUM(valor) as soma FROM transacao WHERE FK_usuario_c = '@Debor4';
