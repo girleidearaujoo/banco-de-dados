9bdec12fc0b3e0c982eec2700051f632bdbe7fb7
ellenjaiany <131263420+ellenjaiany@users.noreply.github.com>
2023-10-08 16:42:53 -0300

    Update perguntasDoDavid.sql

--- 3o-bimestre/perguntasDoDavid.sql
+++ 3o-bimestre/perguntasDoDavid.sql
@@ -8,3 +8,4 @@ SELECT * FROM hotel WHERE horario_termino = 22;
 # Quais petshops um determinado animal de um determinado usuários ele já foi
 
 # Quanto um determinado usuário já gastou no chatpet e quais foram as transações em ordem decrescente de valor
+SELECT SUM(valor) as soma FROM transacao WHERE FK_usuario_c = 'Debor4'
