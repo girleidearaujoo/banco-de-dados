8a91b53d996b415e14dce21d35c86f4a72a0aaa2
Girleide de Araujo <115583142+girleidearaujoo@users.noreply.github.com>
2023-10-08 19:18:55 -0300

    Update ChatPet_querys-gui.sql

--- 3o-bimestre/ChatPet_querys-gui.sql
+++ 3o-bimestre/ChatPet_querys-gui.sql
@@ -1,1 +1,17 @@
-
+# Cadastros
+INSERT INTO 
+cliente(nome, email, endereco, usuario, telefone) 
+VALUES (
+"Enzo Duarate", "enzoduarte@gmail.com", "Rua da baguet", "@Enzo1", '(82) 9997-2891'
+);
+INSERT INTO 
+empreendedor(nome, email, endereco, usuario, telefone) 
+VALUES (
+"Pedro Camiro", "suport.vetpet@gmail.com", "Rua da Maria", "@vetPet", '(82) 9087-8675'
+);
+# Publicacoes
+SELECT * FROM publicacao;
+# Info de perfil
+SELECT * FROM cliente;
+# Info das empresas
+SELECT * FROM empreendedor;
